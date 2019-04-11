d9731ebabcf58e24e33e8203005c08d65c0a6213
Karol Kosoń <karol.koson@motorolasolutions.com>
2019-04-11 16:24:11 +0200

    course project

--- course-project-app/src/app/app.module.ts
+++ course-project-app/src/app/app.module.ts
@@ -2,19 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
