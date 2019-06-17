fc87e5a5950a416fadf1efd8ec9959c14c05a80e
Karol Kosoń <karol.koson@motorolasolutions.com>
2019-06-17 17:46:23 +0200

    routers

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -11,22 +11,8 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import {RouterModule, Routes} from '@angular/router';
-
-const appRoutes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'users',
-    component: UserComponent
-  },
-  {
-    path: 'servers',
-    component: ServersComponent
-  },
-];
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -36,13 +22,14 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent],
