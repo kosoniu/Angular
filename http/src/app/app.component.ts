dba01694bc9beec6039e3b00eb2093a5631d2bd7
Karol Kosoń <karol.koson@motorolasolutions.com>
2019-06-26 16:30:19 +0200

    http

---
+++ http/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from "rxjs/operators";
+import { Post } from './post.model';
+import {PostsService} from "./posts.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+  isFetching = false;
+
+  constructor(private http: HttpClient, private postsService: PostsService) {}
+
+  ngOnInit() {
+    this.postsService.fetchPosts();
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.postsService.createAndStorePost(postData.title, postData.content);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.postsService.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+}
